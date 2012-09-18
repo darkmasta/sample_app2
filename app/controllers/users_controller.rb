d0f66a90711480a4c8d3bd38265ddfde49e11a0a
darkmasta <alperen_bagci@hotmail.com>
2012-09-18 23:24:41 +0300

    Completed signin/out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,23 @@
 class UsersController < ApplicationController
-    
-  def show 
+  
+  def show
     @user = User.find(params[:id])
     @title = @user.name
   end
 
   def new
-    @user  = User.new 
+    @user  = User.new
     @title = "Sign up"
   end
-
+  
   def create
     @user = User.new(params[:user])
     if @user.save
-      redirect_to user_path(@user) , :flash => { :success => "Welcome to the Sample App!" }
+      sign_in @user
+      redirect_to @user, :flash => { :success => "Welcome to the Sample App!" }
     else
       @title = "Sign up"
       render 'new'
     end
   end
-
 end
